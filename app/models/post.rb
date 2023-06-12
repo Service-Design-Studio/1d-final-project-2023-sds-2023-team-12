7ae17feabdbea299cbf2a2873dc4b2fee39f2681
Louis Anh Tran <louistrannamanh@gmail.com>
2023-06-13 01:37:34 +0800

    add css for homepage, nav bar, post, show all posts, change logic for login

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
     belongs_to :user
     include ImageUploader::Attachment(:image)
+
+    validates :full_name, presence: true
+    validates :age, presence: true
+    validates :location, presence: true
+    validates :description, presence: true
+    validates :special_note, presence: true
+    validates :image_data, presence: true
+
 end
