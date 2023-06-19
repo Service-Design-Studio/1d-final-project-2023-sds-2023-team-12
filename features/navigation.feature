c4d801328839aef08a799aeadc0b90e8c96c8340
Ayu <ayupermata19602@gmail.com>
2023-06-19 17:45:22 +0800

    added scenario for not logged in and step definition for sad path

--- features/navigation.feature
+++ features/navigation.feature
@@ -1,6 +1,12 @@
 # Cucumber Scenario for "Navigating to Report case Page feature"
 # Step definition file created, check 'features/step_definitions/navigation.rb'
 
+Feature: Ensuring user is not logged in
+  Scenario: User is not logged in
+  Given I am on the page with path: 'Home'
+  Then I should see the login button on the 'navbar'
+  And I should not see the logout button on the 'navbar'
+
 Feature: Navigating to the Report Case Page
 
   As a user
@@ -17,9 +23,9 @@ Feature: Navigating to the Report Case Page
     Then I should be redirected to the page with path :'report_case'
 
 
-    # Sad path in navigating report page (Ayu)
-    Scenario: Navigating to report_case #user is not logged in 
-        Given I am not logged in 
-        And on the page with path: 'home'
-        When I select 'Report Case' on the 'Navigation Bar'
-        Then I should be redirected to the page with path: "login"
+  # Sad path in navigating report page (Ayu)
+  Scenario: Navigating to report_case #user is not logged in 
+    Given I am not logged in 
+    And on the page with path: 'Home'
+    When I select 'Report Case' on the 'navbar'
+    Then I should be redirected to the page with path: "login"
--- features/step_definitions/navigation.rb
+++ features/step_definitions/navigation.rb
@@ -1,3 +1,24 @@
+#Identifying if a person if a person is logged in/not logged in
+
+#Given I am on the page with path: 'Home'
+Given 'I am on the page with path: {string}' do |path|
+    visit(path)
+end
+
+#Then I should see the login button on the 'navbar'
+Then 'I should see the login button on {string}' do |element|
+    within(element) do
+        expect(page).to have_selector("button", text "Login")
+    end
+end
+
+#And I should not see the logout button
+Then "I should not see the logout button" do |element| 
+    within(element) do
+        expect(page).not_to have_selector("button", text:"Logout")
+    end
+end
+
 # Happy Path in navigating to report page
 # Yixuan's part (Incomplete)
 
@@ -23,3 +44,28 @@ Then 'I should be redirected to the page with path {string}' do |action|
     # Check if the current path matches the provided path using the 'expect' assertion.
     expect(current_path).to eq "/#{action}"
 end
+
+#Sad Path in navigating to report page (Ayu)
+
+#Scenario: Navigating to report_case #user is not logged in 
+# Given I am not logged in 
+# And on the page with path: 'home'
+Given 'I am not logged' do
+    #Add code to ensure user is not logged in, idk how to do yet
+end
+
+Given 'I am on the page with path: {string}' do |path|
+    visit(path)
+end
+
+#When I select 'Report Case' on the 'Navigation Bar'
+When 'I select {string} on the {string}' do |path|
+    within(element) do
+        click_link_or_button path
+    end
+end
+
+#Then I should be redirected to the page with path: "login"
+Then 'I should be redirected to the page with path: {string}' do |path|
+    expect(current_path).to eq(path)
+end 
